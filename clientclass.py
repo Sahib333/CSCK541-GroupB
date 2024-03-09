594873111e6ea282f0a2f9544cc027cf58ff16f7
minkim8 <107504493+minkim8@users.noreply.github.com>
2024-03-10 00:05:32 +0900

    I added the two parameters on serverclass.py as 'print_screen','save_file' but it seems to only work for 'save' for 'text' files. I just don't have time anymore to look into it. Can anyone check if this is going to a correct way?

--- clientclass.py
+++ clientclass.py
@@ -8,45 +8,43 @@ from cryptography.fernet import Fernet
 
 class Client:
     """Client class"""
-    def __init__(self, host, port):
+    def __init__(self, host, port, print_screen, save_file):
         self.host = host
         self.port = port
         self.client_socket = socket.socket()
         self.client_socket.connect((host, port))
-        self.fernet_key = Fernet.generate_key()
-        self.fernet = Fernet(self.fernet_key)
+        self.print_screen = print_screen
+        self.save_file = save_file
 
     def send_dictionary (self, data_format, data):
-        """Send dictionary"""
+        """Class method to send dictionary"""
+        # Serialise dictionary
         serialized_data = self.serialize_dictionary(data_format, data)
-        print(serialized_data)
-        print(type(serialized_data))
-        msg = f"dictionary,{data_format},{serialized_data}"
-        #Encoding the string before sending it
-        self.client_socket.send(msg.encode())
 
-    def send_text (self, data, encrypt):
-        """Send text"""
-        #if encrypt:
-        #    data = self.encrypt_data(data)
-        if encrypt:
-            encrypted_data = self.fernet.encrypt(data.encode())
-            msg = f"text,{encrypted_data.decode()},{self.fernet_key.decode()}"
+        if data_format == "binary":
+            # if the data format is binary we don't need to encode it
+            msg = f"dictionary\n{data_format}\n"
+            enconded_msg = msg.encode() + serialized_data
         else:
-            msg = f"text,{data},{str(encrypt)}"
+            # Create string with serialized dictionary and data format
+            msg = f"dictionary\n{data_format}\n{serialized_data}"
+            # Encode the string before sending it
+            enconded_msg = msg.encode()
+
+        # Encode the string before sending it
+        self.client_socket.send(enconded_msg)
+
+    def send_textfile (self, file_path, encrypted):
+        """Send text file"""
+        #if encrypted:
+        #    data = self.encrypt_data(data)
+
+        with open(file_path, "r") as file:
+            content = file.read()
+
+        msg = f"textfile\n{content}\n{encrypted}"
+        print(msg)
         self.client_socket.send(msg.encode())
-
-    def upload_text_file(self, file_path, encrypt=False):
-        """Upload a text file"""
-        with open(file_path, 'rb') as file:
-            file_data = file.read()
-            if encrypt:
-                encrypted_data = self.fernet.encrypt(file_data)
-                msg = f"file,{file_path},{encrypted_data.decode()},{self.fernet_key.decode()}"
-            else:
-                msg = f"file,{file_path},{file_data.decode()},False"
-            self.client_socket.send(msg.encode())
-
 
     #def encrypt_data(self, data):
     #    """Encrypt data"""
@@ -55,29 +53,32 @@ class Client:
     #    return fernet.encrypt(data)
 
     def serialize_dictionary (self, data_format, dictionary):
-        """Serialize the data received by the client depending on its format"""
+        """Serialize the dictionary before sending it depending on its format"""
         if data_format == "binary":
             return pickle.dumps(dictionary)
         elif data_format == "json":
             return json.dumps(dictionary)
         elif data_format == "xml":
-            root = ET.Element("root")
-            for key, value in dictionary.items():
-                ET.SubElement(root,key).text = str(value)
-            return ET.tostring(root)
+            # Helper function to convert dictionary to XML
+            def dict_to_xml(tag, d):
+                elem = ET.Element(tag)
+                for key, value in d.items():
+                    child = ET.Element(key)
+                    child.text = str(value)
+                    elem.append(child)
+                return elem
+
+            xml_data = dict_to_xml("data", dictionary)
+            xml_str = ET.tostring(xml_data, encoding="unicode")
+            return xml_str
 
 
 if __name__ == "__main__":
     HOST = "127.0.0.1"
     PORT = 12345
 
-    client = Client(HOST, PORT)
+    client = Client(HOST, PORT, print_screen=True, save_file=True)
+    file_path = "C:/Users/82102/PycharmProjects/CSCK541-GroupB/example.txt"
+    client.send_textfile (file_path, False)
     dictionary_data = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
-    client.send_dictionary("json", dictionary_data)
-
-    #Text File
-    TEXT = "hello world!"
-    client.send_text(TEXT, encrypt=True)
-
-    file_path = "example.txt"
-    client.upload_text_file(file_path, encrypt=True)
+    client.send_dictionary("xml", dictionary_data)
--- serverclass.py
+++ serverclass.py
@@ -7,14 +7,15 @@ import xml.etree.ElementTree as ET
 from cryptography.fernet import Fernet
 
 class Server:
-    BUFFER_SIZE = 1024
     """ Server class"""
-    def __init__(self, host, port):
+    def __init__(self, host, port, print_screen = False, save_file = False):
         self.host = host
         self.port = port
         self.server_socket = socket.socket()
         self.server_socket.bind((host, port))
         self.server_socket.listen(5)
+        self.print_screen = print_screen
+        self.save_file = save_file
 
     def connect(self):
         """Connect to the client"""
@@ -28,31 +29,54 @@ class Server:
     def handle_client(self, client_socket):
         """Receive data type (dictionary or text file)"""
         # Converting the data received into a string
-        received_data = client_socket.recv(self.BUFFER_SIZE).decode()
-        print(received_data)
-        msg_parts = received_data.split(",")
-        data_type = msg_parts[0]
+        received_data = client_socket.recv(BUFFER_SIZE)
+
+        # Split the string using \n as delimenter
+        msg_parts = received_data.split(b"\n")
+
+        # Check if msg is text or dictionary
+        data_type = msg_parts[0].decode()
 
         if data_type == "dictionary":
-            print("Data type: dictionary")
-            data_format = msg_parts[1]
-            data = str(msg_parts[2:])
-            print(data)
-            print(type(data))
+            print("Data type: Dictionary")
 
+            # Separate remaining parts of the string
+            data_format = msg_parts[1].decode()
+            data = msg_parts[2]
+
+            # Deserialize dictionary
             dictionary = self.deserialize_dictionary (data, data_format)
             print("Dictionary received")
 
-            # Save to a file
-            with open("received_dictionary.txt","w", encoding="utf-8") as my_file:
-                my_file.write(str(dictionary))
+            # Print
+            if self.print_screen:
+                print("Received data:", dictionary)
 
-        elif data_type == "text":
-            data = msg_parts[1]
-            key = msg_parts[2]
-            decrypted = self.decrypt_data(data, key)
-            print("Encrypted text:", data)
-            print("Decrypted text:", decrypted)
+            # Save to a file
+            if self.save_file:
+                with open("received_dictionary.txt","w", encoding="utf-8") as my_file:
+                    my_file.write(str(dictionary))
+
+        elif data_type == "textfile":
+            print("Data type: Text file")
+
+            # Separate remaining parts of the string
+            data = msg_parts[1].decode()
+            encrypted_str = msg_parts[2].decode()
+            # Convert "True" or "False" string to boolean
+            encrypted = encrypted_str.lower() == "true"
+            print("Received data:", data)
+            print("Data length:", len(data))
+            print("Encrypted:", encrypted)
+
+            # Print
+            if self.print_screen:
+                print("Received data:", data)
+
+            # Save to a file
+            if self.save_file:
+                with open("received_text.txt","w", encoding="utf-8") as my_file:
+                    my_file.write(data)
 
             #if encrypted:
             #    # Decrypt data
@@ -60,10 +84,8 @@ class Server:
             #    data = fernet.decrypt(data)
 
             # Save to a file
-            with open("received_text.txt", "wb") as my_file:
-                my_file.write(decrypted.encode())
-            with open("received_encrypted_text.txt", "wb") as encrypted_file:
-                encrypted_file.write(data.encode())
+            #with open("received_text.txt","wb") as my_file:
+            #    my_file.write(data)
 
     def deserialize_dictionary (self, data, data_format):
         """Deserialize the data received by the client depending on its format"""
@@ -74,12 +96,29 @@ class Server:
             return json.loads(data)
 
         elif data_format == "xml":
-            return ET.fromstring(data)
+            # Helper functions to convert XML to dictionary
+            def xml_to_dict(data):
+                root = ET.fromstring(data)
+                return _xml_to_dict_helper(root)
+
+            def _xml_to_dict_helper(element):
+                result = {}
+                for child in element:
+                    if len(child):
+                        result[child.tag] = _xml_to_dict_helper(child)
+                    else:
+                        result[child.tag] = child.text
+                return result
+
+            # Deserialize XML to dictionary
+            data_dict = xml_to_dict(data)
+            return data_dict
+
 
 if __name__ == "__main__":
     HOST = "127.0.0.1"
     PORT = 12345
     BUFFER_SIZE = 1024
 
-    server = Server(HOST, PORT)
+    server = Server(HOST, PORT, print_screen=True, save_file=True)
     server.connect()

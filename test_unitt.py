3ca69dd2f7432d7ef56f1ebef1f5e30bde5f11ac
Penger13 <120578063+Penger13@users.noreply.github.com>
2024-03-12 20:47:58 -0400

    Add files via upload

--- test_unitt.py
+++ test_unitt.py
@@ -38,31 +38,39 @@ class TestServerClient(unittest.TestCase):
             result = client.validate_encrypt_option(option)
             self.assertFalse(result, f"Expected '{option}' to be invalid, but it is not.")
 
-    def test_server_client_communication(self):
-        # start the server in a separate thread
-        server_thread = threading.Thread(target=self.start_server)
-        server_thread.start()
+class TestServerClientCommunication(unittest.TestCase):
 
-        # ensure that the server is running
-        server_ready = False
-        while not server_ready:
-            try:
-                client = Client(self.HOST, self.PORT)
-                server_ready = True
-            except ConnectionRefusedError:
-                pass
+        def setUp(self):
+            self.HOST = "127.0.0.1"
+            self.PORT = 12345
+            self.BUFFER_SIZE = 1024
 
-        # test sending dictionary
-        dictionary_data = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
-        client.send_dictionary("json", dictionary_data)
+        def test_server_client_communication(self):
+            # Start the server in a separate thread
+            server_thread = threading.Thread(target=self.start_server)
+            server_thread.start()
 
-        # test sending text file
-        file_path = "test.txt"
-        client.send_textfile(file_path, False)
+            # Ensure that the server is running
+            server_ready = False
+            while not server_ready:
+                try:
+                    client = Client(self.HOST, self.PORT)
+                    server_ready = True
+                except ConnectionRefusedError:
+                    pass
 
-    def start_server(self):
-        server = Server(self.HOST, self.PORT)
-        server.connect()
+            # Test sending dictionary
+            dictionary_data = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
+            client.send_dictionary("json", dictionary_data)
+
+            # Test sending text file
+            file_path = "test.txt"
+            client.send_textfile(file_path, False)
+
+        def start_server(self):
+            server = Server(self.HOST, self.PORT)
+            server.connect()
+
 
 class TestEncryption(unittest.TestCase):
 
